c5c63a5c73414a99dc15daa79c5c62fce65f8fcc
Santhosh-OFFX <Santhosh.s322006@gmail.com>
2026-07-02 15:12:21 +0530

    binary search.java

---
+++ binary search.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] n, int t) {
+        int low=0;
+        int high=n.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(t==n[mid]) return mid;
+            else if(t>n[mid]) low=mid+1;
+            else high=mid-1;
+        }
+        return -1;
+    }
+}
