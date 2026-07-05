965f8c5bd65c2b86d227c12d7f94e274bc82e8da
Santhosh-OFFX <Santhosh.s322006@gmail.com>
2026-07-05 21:47:21 +0530

    minimum moves to equal array elements.java

---
+++ minimum moves to equal array elements.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minMoves(int[] nums) {
+           int max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+
+        int moves = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            moves += (max - nums[i]);
+        }
+
+        return moves;
+    }
+}
